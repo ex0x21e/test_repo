e569d141c7758544d4917e5ed0c09f97a69f4453
ex0x21e <119272433+ex0x21e@users.noreply.github.com>
2025-09-02 15:51:51 +1200

    m

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 let a = 100;
 <<<<<<< Updated upstream
 let c = 300
 =======
 let b = 200;
 >>>>>>> Stashed changes
+=======
+const hello()=>console.log("Hello World");
+>>>>>>> parent of 4a1aca4 (Update hello.js)
